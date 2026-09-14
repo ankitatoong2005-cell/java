461b71d90f76c4bd8b71e02e483767415011df3b
ankitatoong2005-cell <ankitatoong2005@gmail.com>
2026-09-14 09:55:05 +0530

    Add files via upload

---
+++ elseif.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Variable declaration
+        int marks = 60;
+
+        // Check multiple conditions using else if ladder
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } 
+        else if (marks >= 70) {
+            System.out.println("Grade B");
+        } 
+        else if (marks >= 50) {
+            System.out.println("Grade C");
+        } 
+        else {
+            System.out.println("Grade D");
+        }
+    }
+}
